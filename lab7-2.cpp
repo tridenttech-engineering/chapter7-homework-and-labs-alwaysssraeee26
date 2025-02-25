39aeb33dcae98e1479ef1505abaae673df75f757
alwaysssraeee26 <raechatt06@gmail.com>
2025-02-25 15:30:51 -0500

    Submission lab7-2.cpp

--- lab7-2.cpp
+++ lab7-2.cpp
@@ -1,14 +1,28 @@
-//Lab7-2.cpp - Displays the number of years required
-//for a company's sales to reach at least $150,000
-//using a 5.5% annual growth rate. Also displays
-//the sales at that time.
-//Created/revised by <your name> on <current date>
+// Lab7-2.cpp - Displays the number of years required
+// for a company's sales to reach at least $150,000
+// using a 5.5% annual growth rate. Also displays
+// the sales at that time.
+// Created/revised by <your name> on <current date>
 
 #include <iostream>
 #include <iomanip>
-using namespace std;
 
-int main()
-{
+int main() {
+    const double GROWTH_RATE = 0.055;
+    double sales = 0.0;
+    int years = 0;
+
+    std::cout << "Current year's sales: ";
+    std::cin >> sales;
+
+    while (sales < 150000.0) {
+        sales += sales * GROWTH_RATE;
+        years++;
+    }
+
+    std::cout << std::fixed << std::setprecision(2);
+    std::cout << "Sales " << years << " years from now: $" << sales << std::endl;
+
     return 0;
-} //end of main function
+}
+
