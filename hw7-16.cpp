b44d355c5bcb55389e787fdec11082d5910b621a
alwaysssraeee26 <raechatt06@gmail.com>
2025-02-25 16:30:32 -0500

    Submission hw7-16.cpp

--- hw7-16.cpp
+++ hw7-16.cpp
@@ -1,5 +1,5 @@
-//hw7-16.cpp - displays three tip amounts
-//Created/revised by <your name> on <current date>
+// hw7-16.cpp - displays three tip amounts
+// Created/revised by <your name> on <current date>
 
 #include <iostream>
 #include <iomanip>
@@ -7,20 +7,20 @@ using namespace std;
 
 int main()
 {
-	double bill = 0.0;
-	double tip = 0.0;
+    double bill = 0.0;
+    double tip = 0.0;
 
-	cout << "Bill amount: ";
-	cin >> bill;
+    cout << "Bill amount: ";
+    cin >> bill;
 
-	cout << fixed;
-	for (double rate = 0.1; rate <= 0.2; rate += 0.05)
-	{
-		tip = bill * rate;
-		cout << setprecision(0);
-		cout << rate * 100 << "% tip: ";
-		cout << setprecision(2);
-		cout << "$" << tip << endl;
-	}   // end for
-	return 0;
-}	//end of main function
+    cout << fixed << setprecision(2);  // Ensures that both the tip and bill are displayed with two decimal places
+
+    // Loop through 10%, 15%, and 20% tip rates
+    for (double rate = 0.10; rate <= 0.20; rate += 0.05)
+    {
+        tip = bill * rate;
+        cout << rate * 100 << "% tip: $" << tip << endl;
+    }
+
+    return 0;
+}   // end of main function
