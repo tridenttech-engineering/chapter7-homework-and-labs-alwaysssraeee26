78113d399557bc126979180951f854acf193db6f
alwaysssraeee26 <raechatt06@gmail.com>
2025-02-25 15:34:29 -0500

    Submission lab7-1.cpp

--- lab7-1.cpp
+++ lab7-1.cpp
@@ -7,7 +7,22 @@
 using namespace std;
 
 int main()
-{	
+{
+    int totalTexts = 0;
+    int dailyTexts = 0;
+    double average = 0.0;
+
+    for (int day = 1; day <= 7; day++) // Loop for 7 days
+    {
+        cout << "How many text messages did you send on day " << day << "? ";
+        cin >> dailyTexts;
+        totalTexts += dailyTexts;
+    }
+
+    average = static_cast<double>(totalTexts) / 7.0; // Divide by 7 explicitly
+
+    cout << fixed << setprecision(1); // Set precision to 1 decimal place
+    cout << endl << "You sent approximately " << average << " text messages per day." << endl;
 
     return 0;
-}   //end of main function
+}
